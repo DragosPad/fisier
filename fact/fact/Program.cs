e18bea49e078522c1a39b9e9dde63e5f483bfdb8
DragosPad <dragospaduraru89@yahoo.com>
2017-04-25 16:53:01 +0300

    Add files via upload

---
+++ fact/fact/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, c, fact;
+            System.Console.WriteLine("number is: ");
+            n = int.Parse(Console.ReadLine());
+            fact = n;
+            for (c = n - 1; c >= 1; c--)
+            {
+                fact = fact * c;
+            }
+            Console.WriteLine("The number you entered was {0} and it's factorial is {1}", n, fact);
+            Console.ReadLine();
+        }
+    }
+}
