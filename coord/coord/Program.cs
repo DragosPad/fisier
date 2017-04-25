562843b160d5d1fd9eb6a6d7fc6972716b2f6384
DragosPad <dragospaduraru89@yahoo.com>
2017-04-25 16:47:40 +0300

    Add files via upload

---
+++ coord/coord/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coord
+{
+
+    public struct Coordinate
+    {
+        public int x;
+        public int y;
+    }
+
+    class EntryPoint
+    {
+        public static void AttemptToModifyCoord(Coordinate coord)
+        {
+            coord.x = 1;
+            coord.y = 3;
+        }
+        public static void ModifyCoord(ref Coordinate coord)
+        {
+            coord.x = 10;
+            coord.y = 10;
+        }
+        static void Main(string[] args)
+        {
+
+            Coordinate location;
+            location.x = 50;
+            location.y = 50;
+
+            AttemptToModifyCoord(location);
+            System.Console.WriteLine("( {0}, {1} )", location.x, location.y);
+           // Console.ReadLine();
+
+            ModifyCoord(ref location);
+            System.Console.WriteLine("( {0}, {1} )", location.x, location.y);
+            Console.ReadLine();
+        }
+    }
+}
