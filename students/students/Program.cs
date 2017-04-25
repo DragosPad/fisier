cbe4ff2273d82e8f36cf07032bdb612624846451
DragosPad <dragospaduraru89@yahoo.com>
2017-04-25 16:53:50 +0300

    Add files via upload

---
+++ students/students/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User[] users = new User[5] { new User("Tom" , 23),  
+                                         new User("Max" , 20),
+                                         new User("Eli" , 25),
+                                         new User("Marve" , 22),
+                                         new User("Adi" , 19)};
+            
+                
+                foreach (User user in users)
+                {
+                    Console.WriteLine( user.Name + user.Age + " ");
+                }
+
+
+                int maxAge = users.Max(user => user.Age);
+
+                foreach (User user in users)
+                {
+                    if (user.Age > maxAge)
+                    {
+                        maxAge = user.Age;
+                    }
+                   
+                }
+                Console.WriteLine("Age maxim {0}", maxAge);
+
+
+                int minAge = users.Max(user => user.Age);
+
+                foreach (User user in users)
+                {
+                    if (user.Age < minAge)
+                    {
+                        minAge = user.Age;
+                    }
+                   
+                }
+                Console.WriteLine("Age minim {0}", minAge);
+            
+
+            Console.ReadLine();
+
+        }
+    }
+
+}
